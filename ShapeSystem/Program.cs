c7565f10615b5aa73da829f1b27e17fe3aed7a2d
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-22 18:57:41 +0500

    Draw methods were called and used.

--- ShapeSystem/Program.cs
+++ ShapeSystem/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ShapeSystem.Models;
+
+namespace ShapeSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Shape shape = new Shape();
+        Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle();
+        Triangle triangle = new Triangle();
+
+        shape.Draw();
+        circle.Draw();
+        rectangle.Draw();
+        triangle.Draw();
+    }
+}
