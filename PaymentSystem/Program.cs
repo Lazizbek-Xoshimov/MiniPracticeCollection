fd969d9ff4a40f125efb8121f6b15a81f2a2a833
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-22 19:39:15 +0500

    Pay method were called and used

--- PaymentSystem/Program.cs
+++ PaymentSystem/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using PaymentSystem.Models;
+
+namespace PaymentSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Payment clickPayment = new ClickPayment();
+        Payment paymePayment = new PaymePayment();
+        Payment cashPayment = new CashPayment();
+
+        clickPayment.Pay();
+        paymePayment.Pay();
+        cashPayment.Pay();
+    }
+}
