f5eeb66dd19c635cf564ea1d53f9f0f975dae3b0
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-22 20:06:24 +0500

    TurnOn methods were called and used

--- SmartDevices/Program.cs
+++ SmartDevices/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SmartDevices.Models;
+
+namespace SmartDevices;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Device phone = new Phone();
+        Device laptop = new Laptop();
+        Device tv = new TV();
+
+        phone.TurnOn();
+        laptop.TurnOn();
+        tv.TurnOn();
+    }
+}
