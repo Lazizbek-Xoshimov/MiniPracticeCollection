fa8a3fdaedf5e8628c8153dfd3b3b729cc2abd21
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-22 19:24:41 +0500

    Send methods were called and used.

--- NotificationSystem/Program.cs
+++ NotificationSystem/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using NotificationSystem.Models;
+
+namespace NotificationSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        EmailNotification emailNotification = new EmailNotification();
+        SmsNotification smsNotification = new SmsNotification();
+        TelegramNotification telegramNotification = new TelegramNotification();
+
+        emailNotification.Send();
+        smsNotification.Send();
+        telegramNotification.Send();
+    }
+}
