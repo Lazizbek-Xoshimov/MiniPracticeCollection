19840cfee367c550fdb76b9cbe503a49a33cbc5b
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-22 19:53:47 +0500

    Attack methods were called and used

--- GameCharacters/Program.cs
+++ GameCharacters/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using GameCharacters.Models;
+
+namespace GameCharacters;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Character warrior = new Warrior("Warrior");
+        Character archer = new Archer("Archer");
+        Character mage = new Mage("Mage");
+
+        warrior.Attack();
+        archer.Attack();
+        mage.Attack();
+    }
+}
